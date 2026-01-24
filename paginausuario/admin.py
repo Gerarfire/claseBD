2118d5fb512bc3b33820a25c6493fd3fc4d145c3
Gerarfire <gerarfire023@gmail.com>
2026-01-24 01:17:48 -0500

    cambio 6

--- paginausuario/admin.py
+++ paginausuario/admin.py
@@ -23,20 +23,95 @@ class DatosPersonalesAdmin(admin.ModelAdmin):
 
 @admin.register(ExperienciaLaboral)
 class ExperienciaLaboralAdmin(admin.ModelAdmin):
-    list_display = ('cargodesempenado', 'nombrempresa', 'fechainiciogestion', 'fechafingestion')
+    list_display = ('cargodesempenado', 'nombrempresa', 'fechainiciogestion', 'fechafingestion', 'activarparaqueseveaenfront')
+    list_filter = ('activarparaqueseveaenfront', 'fechainiciogestion', 'fechafingestion')
+    list_editable = ('activarparaqueseveaenfront',)
+    search_fields = ('cargodesempenado', 'nombrempresa', 'descripcionfunciones')
+    ordering = ('-fechainiciogestion',)
+    actions = ['activar_items', 'desactivar_items']
+
+    def activar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=True)
+        self.message_user(request, f"{queryset.count()} items activados.")
+    activar_items.short_description = "Activar items seleccionados"
+
+    def desactivar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=False)
+        self.message_user(request, f"{queryset.count()} items desactivados.")
+    desactivar_items.short_description = "Desactivar items seleccionados"
 
 @admin.register(Reconocimiento)
 class ReconocimientoAdmin(admin.ModelAdmin):
-    list_display = ('tiporeconocimiento', 'descripcionreconocimiento', 'entidadpatrocinadora', 'fechareconocimiento')
+    list_display = ('tiporeconocimiento', 'descripcionreconocimiento', 'entidadpatrocinadora', 'fechareconocimiento', 'activarparaqueseveaenfront')
+    list_filter = ('activarparaqueseveaenfront', 'tiporeconocimiento', 'fechareconocimiento')
+    list_editable = ('activarparaqueseveaenfront',)
+    search_fields = ('tiporeconocimiento', 'descripcionreconocimiento', 'entidadpatrocinadora')
+    ordering = ('-fechareconocimiento',)
+    actions = ['activar_items', 'desactivar_items']
+
+    def activar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=True)
+        self.message_user(request, f"{queryset.count()} items activados.")
+    activar_items.short_description = "Activar items seleccionados"
+
+    def desactivar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=False)
+        self.message_user(request, f"{queryset.count()} items desactivados.")
+    desactivar_items.short_description = "Desactivar items seleccionados"
 
 @admin.register(CursoRealizado)
 class CursoRealizadoAdmin(admin.ModelAdmin):
-    list_display = ('nombrecurso', 'entidadpatrocinadora', 'fechainicio', 'totalhoras')
+    list_display = ('nombrecurso', 'entidadpatrocinadora', 'fechainicio', 'totalhoras', 'activarparaqueseveaenfront')
+    list_filter = ('activarparaqueseveaenfront', 'entidadpatrocinadora', 'fechainicio')
+    list_editable = ('activarparaqueseveaenfront',)
+    search_fields = ('nombrecurso', 'entidadpatrocinadora', 'descripcioncurso')
+    ordering = ('-fechainicio',)
+    actions = ['activar_items', 'desactivar_items']
+
+    def activar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=True)
+        self.message_user(request, f"{queryset.count()} items activados.")
+    activar_items.short_description = "Activar items seleccionados"
+
+    def desactivar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=False)
+        self.message_user(request, f"{queryset.count()} items desactivados.")
+    desactivar_items.short_description = "Desactivar items seleccionados"
 
 @admin.register(ProductoAcademico)
 class ProductoAcademicoAdmin(admin.ModelAdmin):
-    list_display = ('nombrerecurso', 'clasificador', 'descripcion')
+    list_display = ('nombrerecurso', 'clasificador', 'descripcion', 'activarparaqueseveaenfront')
+    list_filter = ('activarparaqueseveaenfront', 'clasificador')
+    list_editable = ('activarparaqueseveaenfront',)
+    search_fields = ('nombrerecurso', 'clasificador', 'descripcion')
+    ordering = ('-nombrerecurso',)
+    actions = ['activar_items', 'desactivar_items']
+
+    def activar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=True)
+        self.message_user(request, f"{queryset.count()} items activados.")
+    activar_items.short_description = "Activar items seleccionados"
+
+    def desactivar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=False)
+        self.message_user(request, f"{queryset.count()} items desactivados.")
+    desactivar_items.short_description = "Desactivar items seleccionados"
 
 @admin.register(ProductoLaboral)
 class ProductoLaboralAdmin(admin.ModelAdmin):
-    list_display = ('nombreproducto', 'fechaproducto', 'descripcion')
+    list_display = ('nombreproducto', 'fechaproducto', 'descripcion', 'activarparaqueseveaenfront')
+    list_filter = ('activarparaqueseveaenfront', 'fechaproducto')
+    list_editable = ('activarparaqueseveaenfront',)
+    search_fields = ('nombreproducto', 'descripcion')
+    ordering = ('-fechaproducto',)
+    actions = ['activar_items', 'desactivar_items']
+
+    def activar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=True)
+        self.message_user(request, f"{queryset.count()} items activados.")
+    activar_items.short_description = "Activar items seleccionados"
+
+    def desactivar_items(self, request, queryset):
+        queryset.update(activarparaqueseveaenfront=False)
+        self.message_user(request, f"{queryset.count()} items desactivados.")
+    desactivar_items.short_description = "Desactivar items seleccionados"
